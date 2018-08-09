b29023b5905cdd4cb3cc70bf143f53e60d3b1d98
Johnny Kekker <giomail.iv@gmail.com>
2018-08-09 16:54:23 +0300

    improve math

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <title>Labwork #1</title>
-
 <form action="" method="post">
 	<p>x: <input type="select" name="x"/></p>
 	<p>y: <input type="text" name="y"/></p>
@@ -10,29 +9,27 @@
 $x = (float)$_POST['x'];
 $y = (float)$_POST['y'];
 $r = (float)$_POST['r'];
-$answer = False;
 function check_coord($x, $y, $r)
 {
-	$answer = False;
-	if ($x > 0 && $y > 0){
-		if (($x^2 + $y^2) < ($r/2))
-			$answer = True;
-		else $answer = False;
-		return $answer;
-	}
+	if ($x == 0 && $y ==0)
+		return True;
+	else if ($x > 0 && $y > 0){
+		if (sqrt((pow($x, 2) + pow($y, 2))) <= ($r/2))
+			return True;
+		else return False;
+	} 
 	else if ($x < 0 && $y > 0){
-		$answer = False;
-		return $answer;
-	}
+		return False;
+	} 
 	else if ($x < 0 && $y < 0){
-		if ($x < ($y/2))
-			$answer = True;
-		return $answer;
+		if (-$x <= $r - 2*(-$y))
+			return True;
+		else return False;
 	}
 	else if ($x < $r && $y < $r){
-		$answer = True;
-		return $answer;
-	}
+		return True;
+	} 
+	else return False;
 }
 echo "answer is ";
 var_export(check_coord($x, $y, $r));

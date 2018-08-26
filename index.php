98666ba85c70dd9f1a50b92ca75a1265871099ee
Johnny Kekker <giomail.iv@gmail.com>
2018-08-26 03:00:04 +0300

    result table instead label

--- index.php
+++ index.php
@@ -33,18 +33,48 @@
 				<option>2.5</option>
 				<option>3</option>
 			</select>
-			<?php 
+			<?php
+				$currentTime = date("H:i:s", strtotime('-1 hour'));
 				include 'check.php';
+				$start = microtime(true);
 				$x = (float)$_POST['x'];
 				$y = (float)$_POST['y'];
 				$r = (float)$_POST['r'];
+				$res =check_coord($x, $y, $r);
+				$time = microtime(true) - $start;
 			?>
 			<p><input type="submit" onclick="validateForm()" class="option" value="Ok" name="button" id='ok'></p>
-			<div id='res'>Answer: <?php var_export(check_coord($x, $y, $r)) ?></div>
 		</form>
+		<table id='result'>
+			<caption>Results</caption>
+			<tr>
+				<th>X</th>
+				<th>Y</th>
+				<th>R</th>
+				<th>Result</th>
+				<th>Time</th>
+				<th>Script time</th>
+			</tr>
+			<tr>
+				<th><?php echo $x ?></th>
+				<th><?php echo $y ?></th>
+				<th><?php echo $r ?></th>
+				<th><?php echo var_export($res) ?></th>
+				<th><?php echo $currentTime ?></th>
+				<th><?php echo number_format($time, 10, ".", "") ?></th>
+			</tr>
+		</table>
 	</body>
 </html>
 <script type="text/javascript">
+	function add_result_to_table() {
+		var table = document.getElementById('result');
+		var row = table.insertRow(0);
+		var cell1 = row.insertCell(0);
+		var cell2 = row.insertCell(1);
+		cell1.innerHTML = "NEW CELL1";
+		cell2.innerHTML = "NEW CELL2";
+	}
 	function validateForm() {
     	var values = document.forms["myForm"];
     	for(var key in Object.keys(values)){
@@ -53,6 +83,7 @@
   				if (isNaN(value))
   					alert('\'' + value + '\' is not a number');
 		}
+		add_result_to_table();
 		return false;
-	} 
+	}
 </script>

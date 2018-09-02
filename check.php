7ccf81c3736ef4ac375922ed940160343713ae8b
Johnny Kekker <giomail.iv@gmail.com>
2018-09-02 18:27:01 +0300

    improved checking script

--- check.php
+++ check.php
@@ -1,24 +1,10 @@
 <?php
 function check_coord($x, $y, $r)
 {
-	if ($x == 0 && $y ==0)
-		return True;
-	else if ($x > 0 && $y > 0){
-		if (sqrt((pow($x, 2) + pow($y, 2))) <= ($r/2))
-			return True;
-		else return False;
-	} 
-	else if ($x < 0 && $y >= 0){
-		return False;
-	}
-	else if ($x < 0 && $y < 0){
-		if (-$x <= $r - 2*(-$y))
-			return True;
-		else return False;
-	}
-	else if ($x < $r && -$y < $r){
-		return True;
-	} 
-	else return False;
+	if (
+		($x >= 0 && $y >= 0 && sqrt((pow($x, 2) + pow($y, 2))) <= ($r/2)) ||
+		($x > 0 && $y < 0 && $x <= $r && -$y <=$r) ||
+		($x <= 0 && $y <= 0 && -$x <= $r - 2*(-$y))) { return True; }
+	else { return False; }
 }
 ?>
